1421495bc74a869a032c8a0b5b3dea740fc67dc0
corytveten <corytveten@gmail.com>
2020-06-24 16:16:27 -0700

    button styling

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,10 @@
-console.log('hello');
+
+//User clicked on the add button
+//if there is any text inside the item field, add that text to the todo list
+document.getElementById('add').addEventListener('click', function() {
+    const value = document.getElementById('item').value;
+    if (value) {
+        
+    }
+    
+});
